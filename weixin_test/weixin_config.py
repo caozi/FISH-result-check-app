0b539f396597bf18b66791c0d6dee8946f93b3cf
caozi <imcaozi@outlook.com>
2020-04-13 08:30:34 +0800

    add HZ

--- weixin_test/weixin_config.py
+++ weixin_test/weixin_config.py
@@ -3,4 +3,4 @@
 TOKEN = 'hellowx'
 appID = 'wx6c11f5e4bbd229bd'
 appsecret = '1605f2bca63385b87ec35daffa2227ea'
-template_ID = 'TjBLnJnwHZCMqFk9vcbRANyUOKTUIDownzX-eWoU_jk'
+template_ID = 'o6LxInrCvxlNlNkqqxbPESlMitEJBFUri-oCtcHDxp0'
