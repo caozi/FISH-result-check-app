9b215dbe545a3a73e2a90c652808f4e433f422d7
caozi <imcaozi@outlook.com>
2020-04-13 08:34:53 +0800

    template modified

--- weixin_test/weixin_config.py
+++ weixin_test/weixin_config.py
@@ -3,4 +3,4 @@
 TOKEN = 'hellowx'
 appID = 'wx6c11f5e4bbd229bd'
 appsecret = '1605f2bca63385b87ec35daffa2227ea'
-template_ID = 'o6LxInrCvxlNlNkqqxbPESlMitEJBFUri-oCtcHDxp0'
+template_ID = 'YQ_BxH4moaWIt24aEtDgdzTKXbvurSWpt1JnME1gnKc'
