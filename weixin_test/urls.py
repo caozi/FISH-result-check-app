11e104b919f31ab043a3065a4e3084e7f0d8aa19
caozi <imcaozi@outlook.com>
2020-05-21 17:33:13 +0800

    去掉多余的代码

--- weixin_test/urls.py
+++ weixin_test/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
         path('back_to_admin_query/', views.back_to_admin_query, name='back_to_admin_query'),
         path('admin_query_override/', views.admin_query_override, name='admin_query_override'),
         path('check_patient_id/', views.check_patient_ID_exist, name='check_patient_id'),
-        path('check_user_name/', views.check_user_name, name='check_user_name'),
 ]
