309f742545ec75fff3509594ac8dfea4b264a99c
caozi <imcaozi@outlook.com>
2020-05-22 11:05:18 +0800

    bug fix

--- weixin_test/views.py
+++ weixin_test/views.py
@@ -165,25 +165,40 @@ def admin_query(request):
 
 @csrf_exempt
 def admin_query_override(request):
-    if request.method == "POST":
-        p_id = request.POST.get('patient_id', '')
-        p_status = request.POST.get('patient_status', '')
-        p_note = request.POST.get('patient_note', '')
-        p = Patient.objects.get(patient_id=p_id)
-        p.patient_status = p_status
-        p.patient_note = p_note
-        p.save()
+    p_id = request.GET.get('patient_id')
+    p_status = request.GET.get('patient_status')
+    p_note = request.GET.get('patient_note')
+    p = Patient.objects.get(patient_id=p_id)
+    p.patient_status = p_status
+    p.patient_note = p_note
+    p.save()
+    try:
         send_message(template_ID, p)
-        return render_to_response('weixin/admin_query_success.html')
-    return HttpResponse('Data not received', content_type="text/plain")
+    except :
+        data = {'notify': False}
+    else:
+        data = {'notify': True}
+    return JsonResponse(data)
+
 
 
 def back_to_admin_query(request):
-    d_openID = request.session.get('openID')
-    doctor = Doctor.objects.get(doctor_openID=d_openID)
-    patients_not_informed = Patient.objects.filter(patient_doctor=doctor).exclude(
-        patient_status='请来报告中心取病理报告').order_by('patient_id')
-    return render(request, 'weixin/admin_patients_not_informed.html', {'patients_not_informed': patients_not_informed})
+    notify = request.POST.get('notify')
+    p_id = request.GET.get('patient_id')
+    if notify == 'success':
+        d_openID = request.session.get('openID')
+        doctor = Doctor.objects.get(doctor_openID=d_openID)
+        patients_not_informed = Patient.objects.filter(patient_doctor=doctor).exclude(
+            patient_status='请来报告中心取病理报告').order_by('patient_id')
+        return render(request, 'weixin/admin_patients_not_informed.html',
+                      {'patients_not_informed': patients_not_informed})
+    else:
+        p = Patient.objects.get(patient_id=p_id)
+        return render(request, 'weixin/notify_by_phone.html', {'patient': p})
+
+
+
+
 
 
 def login_with_oath(request):
