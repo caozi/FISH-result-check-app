598d1b2e99a2cb014f013f7987204f32054972b4
caozi <imcaozi@outlook.com>
2020-05-22 10:15:05 +0800

    change template

--- weixin_test/weixin_config.py
+++ weixin_test/weixin_config.py
@@ -3,4 +3,4 @@
 TOKEN = 'hellowx'
 appID = 'wx6c11f5e4bbd229bd'
 appsecret = '1605f2bca63385b87ec35daffa2227ea'
-template_ID = 'VTl9NdarrtZQnY9P7Ol49mLk1IlyVwHfz9X0fGbn998'
+template_ID = 'xXKmB_vixLBkmwAZ643JLCU4EiCPgqLv1IPHom029W4'
